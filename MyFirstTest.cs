a3dadfc659b6e892f874d75614ee964930199e46
Gerard Singian <gsingian@obph.local>
2025-08-12 11:29:01 +0800

    Initial project setup with a basic Playwright test

---
+++ MyFirstTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace MyPlaywrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class MyFirstTest : PageTest
+{
+    [Test]
+    public async Task ShouldHaveCorrectTitle()
+    {
+        await Page.GotoAsync("https://playwright.dev/");
+        await Expect(Page).ToHaveTitleAsync("Fast and reliable end-to-end testing for modern web apps | Playwright");
+    }
+}
